7f336aa9136cbe156117e61253cbdec35839d98b
Maycon Douglas <mawcom.douglas@gmail.com>
2025-11-13 20:08:24 -0300

    aprendendo html e javascrip

---
+++ src/public/index.js
@@ -0,0 +1,37 @@
+// let nome = "batata"
+// console.log(nome);
+
+// let numero = 13
+// console.log("numero do mal " + numero);
+
+// let telefone;
+// console.log(telefone)
+
+// if (telefone == undefined) {
+//     console.log("indefinido")
+// } else {
+//     console.log("error")
+// }
+
+// let NumeroStr = "1234"
+// let NumeroStrNumero = Number(NumeroStr)
+
+// console.log(typeof NumeroStr)
+// console.log(typeof NumeroStrNumero)
+
+function maior(){
+    let list = [14,55,32,66,2,12,5];
+
+    for (let i = 0; i<list.length; i++ ){
+        console.log("oi")
+        if (list[i]<18){
+            console.log("menor que 18")
+        } else if (list[i]>=18) {
+            console.log("maior que 18")
+        } else {
+            console.log("porra")
+        }
+
+    }
+}
+
